e712cd33ed2a845bda7dcf9b02a6be81fbe20173
Ahmsdkiii <sidqim722@gmail.com>
2025-02-04 11:57:28 +0700

    initialized commit

--- app/page.jsx
+++ app/page.jsx
@@ -7,24 +7,25 @@ import { FiDownload } from "react-icons/fi";
 const Home = () => {
   return (
     <section className="h-full">
-      <div className="container mx-auto h-full">
-        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+      <div className="container h-full mx-auto">
+        <div className="flex flex-col items-center justify-between xl:flex-row xl:pt-8 xl:pb-24">
           {/* text */}
-          <div className="text-center xl:text-left order-2 xl:order-none">
+          <div className="order-2 text-center xl:text-left xl:order-none">
             <span className="text-xl">Software Depelover</span>
-            <h1 className="h1 mb-6">
-              Hello I m <br /> <span className="text-accent">Ahmad Sidqi Mustopa</span>
+            <h1 className="mb-6 h1">
+              Hello I m <br />{" "}
+              <span className="text-accent">Dwi Rahmat Dino</span>
             </h1>
             <p className="max-w-[500px] mb-9 text-white">
               I exel at crafting elegant digital experience and I am proficient
               in various programming languages and technologies.
             </p>
             {/* btn and soacials */}
-            <div className="flex flex-col xl:flex-row items-center gap-8">
+            <div className="flex flex-col items-center gap-8 xl:flex-row">
               <Button
                 variant="outline"
                 size="lg"
-                className="uppercase flex items-center gap-2"
+                className="flex items-center gap-2 uppercase"
               >
                 <span>Download Cv</span>
                 <FiDownload className="text-xl" />
